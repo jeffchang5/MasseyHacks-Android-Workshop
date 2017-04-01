1e2f041d32a7b0512040763a77ffac6d6388fa75
pkkan <kandrupr@msu.edu>
2017-03-31 21:38:35 -0400

    Card View and onClickListener

--- app/src/main/java/xyz/jeffreychang/openweatherlist/recyclerview/WeatherViewHolder.java
+++ app/src/main/java/xyz/jeffreychang/openweatherlist/recyclerview/WeatherViewHolder.java
@@ -1,14 +1,29 @@
 package xyz.jeffreychang.openweatherlist.recyclerview;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 /**
  * Created by jeffreychang on 3/7/17.
  */
 
+
 public class WeatherViewHolder extends RecyclerView.ViewHolder {
+    final String TAG = "WeatherActivity";
+
     public WeatherViewHolder(View itemView) {
         super(itemView);
+
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                int position = getAdapterPosition();
+                Log.d(TAG, Integer.toString(position));
+                // NEW ACTIVITY
+
+            }
+        });
     }
 }
